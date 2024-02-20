f47904ab9a1b1a68796bb0254232b351dc392b80
Dimcheva91 <dimcheva91@gmail.com>
2024-02-20 22:04:35 +0200

    reverse exsercise

---
+++ 01_ReverseStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine() ?? "";
+
+            while (name != "end")
+            {
+                string reversedWord = "";
+                for (int i = name.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += name[i];
+                }
+
+                Console.WriteLine($"{name} = {reversedWord}");
+
+               name = Console.ReadLine()??"";
+            }
+        }
+    }
+}
